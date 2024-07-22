e746197332037bea6360776275cfeaa96b52369f
JemReal <jemreal25@gmail.com>
2024-07-22 14:34:24 -0700

    initial commit: implemented versioning in ASP.NET Core WEb API by folder structure.

---
+++ Web-API-Versioning.API/V2/Controllers/CountriesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Versioning.API.Models.DTOs;
+
+namespace Web_API_Versioning.API.V2.Controllers
+{
+    [Route("api/v2/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countriesDomainModel = CountriesData.Get();
+
+            // Map Domain Model to DTO
+            var response = new List<CountryDtoV2>();
+
+            foreach (var countryDomain in countriesDomainModel)
+            {
+                response.Add(new CountryDtoV2
+                {
+                    Id = countryDomain.Id,
+                    CountryName = countryDomain.Name,
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
